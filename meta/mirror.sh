9b14f5d9143bac8b9289ab10974314d6b6ed9c44
Marcin Skarbek <git@skarbek.name>
2022-07-19 21:07:04 +0200

    feat(meta): split inventories sync

--- meta/mirror.sh
+++ meta/mirror.sh
@@ -41,7 +41,31 @@ pushd ${TMP_DIR}
             fi
         popd
     done
-    #meta is special case
+    #inventories
+    REPO_DIR=$(mktemp -d -p ${TMP_DIR} inventories.XXX)
+    if [ -z ${CI} ]; then
+        git clone --quiet git@${GIT_FORGE}:infrastructure/inventories.git ${REPO_DIR}
+    else
+        git clone --quiet http://gitlab-ci-token:${CI_JOB_TOKEN}@${GIT_FORGE}/infrastructure/inventories.git ${REPO_DIR}
+    fi
+    rsync -a --exclude=".git" ${MIRROR_DIR}/inventories/ ${REPO_DIR}/
+    pushd ${REPO_DIR}
+        if [ ! -z ${CI} ]; then
+            git config user.name "${GIT_NAME}"
+            git config user.email "${GIT_EMAIL}"
+            PUSH_URL=`git remote get-url --push origin | sed "s;:\/\/.*@;:\/\/oauth2:${GIT_TOKEN}@;"`
+            git remote remove origin
+            git remote add origin ${PUSH_URL}
+        fi
+        git add .
+        git diff-index --cached --quiet HEAD --
+        if [ ${?} -ne 0 ]; then
+            echo -e "${TXT_YELLOW}push: sync changes in meta${TXT_CLEAR}"
+            git commit -m 'chore: mirror update'
+            git push --quiet -o ci.skip -u origin main
+        fi
+    popd 
+    #meta
     REPO_DIR=$(mktemp -d -p ${TMP_DIR} meta.XXX)
     if [ -z ${CI} ]; then
         git clone --quiet git@${GIT_FORGE}:infrastructure/meta.git ${REPO_DIR}
